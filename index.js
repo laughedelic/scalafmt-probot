c8b9d5cc4b76f65b0d98dc365948697bd40d9340
Alexey Alekhin <laughedelic@gmail.com>
2018-02-19 02:03:22 +0100

    ♻️ split github utils in a separate module

--- index.js
+++ index.js
@@ -1,98 +1,38 @@
 const scalafmt = require('scalafmt')
+const GHUtils = require('./lib/gh.js')
 
 module.exports = (robot) => {
-  const content = async (context, file) => {
-    const { github, payload } = context
-    const response = await github.repos.getContent(
-      context.repo({
-        path: file,
-        ref: payload.head_commit.id
-      })
-    )
-    const content = Buffer.from(response.data.content, 'base64').toString('utf-8')
-    // robot.log.warn(content)
-    return content
-  }
-
-  const setStatus = async (context, state, descr) => {
-    const { github, payload } = context
-    robot.log(`${state}: ${descr}`)
-
-    github.repos.createStatus(
-      context.repo({
-        sha: payload.head_commit.id,
-        state: state,
-        // target_url: context.payload.head_commit.url,
-        description: descr,
-        context: 'scalafmt'
-      })
-    )
-  }
-
-  const reportSuccess = async (context) => {
-    await setStatus(context, 'success', 'All files are formatted')
-  }
-
-  const reportFailure = async (context, path) => {
-    await setStatus(context, 'failure', `${path} is mis-formatted`)
-  }
-
-  const reportProgress = async (context, count = undefined, total = undefined) => {
-    if (count && total) {
-      const percentage = Math.round(count * 100 / total)
-      await setStatus(context, 'pending', `Checked ${percentage}%`)
-    } else {
-      await setStatus(context, 'pending', 'Preparing')
-    }
-  }
-
-  const reportError = async (context, error) => {
-    await setStatus(context, 'pending', `An error occured: ${error}`)
-  }
-
   robot.on('push', async context => {
-    const { github, payload } = context
-    reportProgress(context)
+    const gh = new GHUtils(robot, context)
 
-    // robot.log(getFiles(context))
+    gh.reportProgress()
 
-    // const files = commit.modified.concat(commit.added)
-    const response = await github.gitdata.getTree(
-      context.repo({
-        sha: payload.head_commit.id,
-        recursive: true
-      })
-    )
-    // robot.log.debug(response.data)
-    const files = response.data.tree
-      .map(node => node.path)
-      .filter(path => path.endsWith('.scala') || path.endsWith('.sbt'))
-    robot.log.debug(files)
-
-    const config = await content(context, '.scalafmt.conf')
+    const config = await gh.getContent('.scalafmt.conf')
+    const files = await gh.listAllFiles()
 
     let count = 0
     const total = files.length
     for (const file of files) {
       try {
         if (count > 0) {
-          reportProgress(context, count, total)
+          gh.reportProgress(count, total)
         }
-        const before = await content(context, file)
+        const before = await gh.getContent(file)
         const after = scalafmt.format(before, file.endsWith('.sbt'), config)
         if (before !== after) {
           robot.log.error(`mis-formatted: ${file}`)
-          await reportFailure(context, file)
+          await gh.reportFailure(file)
           return
         }
         count += 1
       } catch (error) {
         robot.log.error(`${file}: ${error}`)
-        await reportError(context, error)
+        await gh.reportError(error)
         return
       }
     }
-    // process.nextTick(() => reportSuccess(context))
-    await reportSuccess(context)
+
+    // process.nextTick(() => gh.reportSuccess())
+    await gh.reportSuccess()
   })
 }
